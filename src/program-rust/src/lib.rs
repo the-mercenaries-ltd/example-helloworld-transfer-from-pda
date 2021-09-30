19799f31553dfcbdf99d2c2e055c6a1206bf007e
Jack Robson <jackrobsonalpha@gmail.com>
2021-09-30 17:55:28 +0100

    Revert "replace PublicKey.createWithSeed with findProgramAddress but keep createAccountWithSeed"
    This reverts commit 661f203821f8827b08a092e5f3633069c133ea99.

--- src/program-rust/src/lib.rs
+++ src/program-rust/src/lib.rs
@@ -32,9 +32,6 @@ pub fn process_instruction(
     // Get the account to say hello to
     let account = next_account_info(accounts_iter)?;
 
-    msg!("account.owner: {}", account.owner);
-    msg!("program_id: {}", program_id);
-
     // The account must be owned by the program in order to modify its data
     if account.owner != program_id {
         msg!("Greeted account does not have the correct program id");
